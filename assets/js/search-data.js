6141275c453793f338542c2f4d9d11cd488cb519
Ye-Rim-Lee <76906596+Ye-Rim-Lee@users.noreply.github.com>
2025-09-06 02:37:29 +0000

    Deploying to gh-pages from @ Ye-Rim-Lee/Ye-Rim-Lee.github.io@63d818deb6fb4b26a3586d3d6fb4f1c9611eef92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6C%65%65%79%65%31%32@%6D%73%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
